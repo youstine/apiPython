6174a2d548b809a118c98d9e16ea7b6b2f3c8b04
youstine <p.justine.h@gmail.com>
2020-02-28 11:31:54 +0100

    test get

--- Tests/test_get_intervention_by_id.py
+++ Tests/test_get_intervention_by_id.py
@@ -1,6 +1,36 @@
+import json
 from unittest import TestCase
+
+from app import app
+
+
 
 
 class TestGet_intervention_by_id(TestCase):
+
+    def setUp(self):
+        app.config["TESTING"] = True
+        app.config["DEBUG"] = True
+        self.app = app.test_client()
+        self.assertEqual(app.debug, True)
+
     def test_get_intervention_by_id(self):
-        self.fail()
+        # arrange
+        id_intervention = "1"
+        # act
+        result_read = {
+            "client_name": "Laurent",
+            "description": "Le client à une fuite avec sa machine à laver",
+            "id": 1,
+            "intervention_date": "2020/11/01",
+            "intervention_type": "réparation",
+            "tech_name": "Bob"
+        }
+        response = self.app.get('/intervention/' + id_intervention)
+        result = json.loads(response.data)[0]
+        print(result)
+        # assert
+        # self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(result, result_read)
+        # self.fail()
