b2968d8e182041798e11c046c56d5d9d1de0ab6e
zarhkoh <zarhkoh@hotmail.fr>
2020-02-28 11:53:53 +0100

    implementation getAll test

--- Tests/test_get_all_interventions.py
+++ Tests/test_get_all_interventions.py
@@ -1,6 +1,17 @@
+import json
 from unittest import TestCase
+
+from app import app
 
 
 class TestGet_all_interventions(TestCase):
+    def setUp(self):
+        app.config["TESTING"] = True
+        app.config["DEBUG"] = True
+        self.app = app.test_client()
+        self.assertEqual(app.debug, True)
+
     def test_get_all_interventions(self):
-        self.fail()
+        response = self.app.get('/interventions')
+        self.assertEqual(response.status_code, 200, f"test_get_all : Réponse attendue: 200, réponse reçue: {response.status_code}")
+        assert len(response.data) > 0, "test_get_all : the response is empty"
