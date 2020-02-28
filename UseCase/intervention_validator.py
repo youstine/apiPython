cb10a3e172541d6fd4ea0e25b4103dc32c7e1f17
zarhkoh <zarhkoh@hotmail.fr>
2020-02-28 15:05:21 +0100

    trycatch add

--- UseCase/intervention_validator.py
+++ UseCase/intervention_validator.py
@@ -1,24 +1,17 @@
 from Models.intervention import Intervention
+from constantes import CONSTANTE
 
 
 class InterventionValidatorObject:
     def __init__(self, data):
-        if "client_name" not in data:
-            raise Exception("Le nom du client est manquant (client_name)")
-        if "tech_name" not in data:
-            raise Exception("Le nom du technicien est manquant (tech_name)")
-        if "intervention_date" not in data:
-            raise Exception("la date d'intervention est manquante (intervention_date)")
-        if "intervention_type" not in data:
-            raise Exception("le type d'intervention est manquant (intervention_type)")
-        if "description" not in data:
-            raise Exception("la description de l'intervention est manquante (description)")
-        else:
+        try:
             self.client_name = data["client_name"]
             self.tech_name = data["tech_name"]
             self.intervention_date = data["intervention_date"]
             self.intervention_type = data["intervention_type"]
             self.description = data["description"]
+        except Exception as exc:
+            raise Exception(CONSTANTE.INTERV_VALID_ERROR + exc.args[0])
 
     def get_intervention(self):
         return Intervention(self);
